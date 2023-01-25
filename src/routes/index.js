2819dc1d740bfb43a68d5cab65a6509a1ff2f729
Farida-Hamid <faridaaddala@gmail.com>
2023-01-25 14:56:53 +0300

    Fix linter errors

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,20 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Login from "../components/login";
-import ProtectedRoutes from "../components/ProtectedRoutes";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from '../components/login';
+// import ProtectedRoutes from '../components/ProtectedRoutes';
 
-const AppRoutes = () => {
-  return (
-    <Routes>
-      {/* <Route path="/" element={<Home />} />
+const AppRoutes = () => (
+  <Routes>
+    {/* <Route path="/" element={<Home />} />
       <Route path="/instrument_details/:id" element={<Instruments />} /> */}
-      {/* <Route element={<ProtectedRoutes />}> */}
-      {/*  <Route path="/add_instrument" element={<AddInstrument />} />
+    {/* <Route element={<ProtectedRoutes />}> */}
+    {/*  <Route path="/add_instrument" element={<AddInstrument />} />
         <Route path="/delete_instrument/:id" element={<EditInstrument />} /> */}
-      {/* <Route path="/rentals" element={<Rentals />} /> */}
-      {/* </Route> */}
-      <Route path="/login" element={<Login />} />
-      {/* <Route path="/signup" element={<Signup />} /> */}
-    </Routes>
-  );
-};
+    {/* <Route path="/rentals" element={<Rentals />} /> */}
+    {/* </Route> */}
+    <Route path="/login" element={<Login />} />
+    {/* <Route path="/signup" element={<Signup />} /> */}
+  </Routes>
+);
 
 export default AppRoutes;
