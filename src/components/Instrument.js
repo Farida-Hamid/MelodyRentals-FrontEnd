14bec088bd9eef30dc8521e8ab80aff4ed0761c0
Farida-Hamid <faridaaddala@gmail.com>
2023-01-25 19:44:46 +0300

    Display required image props for each instrument

--- src/components/Instrument.js
+++ src/components/Instrument.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
-// const Instrument = ({ props... }) => {
-const Instrument = () => (
-  <h1>🎵 🎶 🎼 </h1>
+const Instrument = ({
+  // key, name, id, description, image, category, price,
+  name, description, image,
+}) => (
+  <div>
+    <h2>{name}</h2>
+    <>.........</>
+    <p>{description}</p>
+    <img
+      src={image}
+      alt={name}
+    />
+  </div>
 );
 
 export default Instrument;
