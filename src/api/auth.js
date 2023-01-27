d60547838807c4c3d7659fe335dd7135e35aecbe
Farida-Hamid <faridaaddala@gmail.com>
2023-01-27 22:05:07 +0300

    Fix API post bug

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,19 +3,25 @@ import axios from "axios";
 const API_URL = "http://127.0.0.1:3000/api/v1/auth/login";
 
 const auth_login = () => {
+  // If you ran the seed file from the BackEnd project, you will have the user loginPayload.
   const loginPayload = {
-    // username: "tm",
-    // name: "tm",
-    email: "tm@localhost",
-    password: "12345678",
-    // password_confirmation: "12345678",
+    "user": {
+      "username": "admin",
+      "name": "Admin",
+      "email": "admin@localhost",
+      "password": "password",
+      "password_confirmation": "password"
+    }
   };
 
-  axios.get(API_URL, loginPayload).then((response) => {
-    // get token from response
-    const t = response.data.token;
-    console.log(response.data);
-  });
+  axios.post(API_URL, loginPayload)
+    .then(response => {
+      //get token from response
+      // const token  =  response.headers.token;
+      console.log("response is =>", (response));
+      // console.log("token is =>", token);
+    })
+    .catch(err => console.log(err));
 };
 
 export default auth_login;
