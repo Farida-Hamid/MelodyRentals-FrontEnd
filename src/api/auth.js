aec97b1bd57b5e2cc77aeac46ce24f0e6692dd3f
Farida-Hamid <faridaaddala@gmail.com>
2023-01-28 15:56:39 +0300

    Find token

--- src/api/auth.js
+++ src/api/auth.js
@@ -6,20 +6,15 @@ const auth_login = () => {
   // If you ran the seed file from the BackEnd project, you will have the user loginPayload.
   const loginPayload = {
     "user": {
-      "username": "admin",
-      "name": "Admin",
       "email": "admin@localhost",
       "password": "password",
-      "password_confirmation": "password"
     }
   };
 
   axios.post(API_URL, loginPayload)
     .then(response => {
-      //get token from response
-      // const token  =  response.headers.token;
-      console.log("response is =>", (response));
-      // console.log("token is =>", token);
+      const token = response.headers.get("Authorization");
+      console.log("token is =>", JSON.stringify(token));
     })
     .catch(err => console.log(err));
 };
