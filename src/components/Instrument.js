a7aff66595b20dcd6efcc6ba032bb9347e0cf7ee
Elie Harfouche <elie7795@gmail.com>
2023-02-01 17:47:03 +0200

    complete details page

--- src/components/Instrument.js
+++ src/components/Instrument.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const Instrument = ({ instrument }) => (
-  <div>
-    <Link to="/details/{id}" state={{ instrument }}>
-      <h2 className="text-decoration-none">{instrument.name}</h2>
-    </Link>
-    <>.........</>
-    <p>{instrument.description}</p>
+  <div className="card border-light">
+    <div className="card-body">
+      <Link to="/details/{id}" state={{ instrument }}>
+        <h2 className="card-title text-center text-decoration-none">{instrument.name}</h2>
+      </Link>
+      <p className="text-muted fw-bold text-center">.........</p>
+      <p className="card-text card-desc ">{instrument.description}</p>
+    </div>
     <img src={instrument.image} alt={instrument.name} />
   </div>
 );
