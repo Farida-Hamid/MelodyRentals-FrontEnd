5921134341625e53ae68db3e403da8d202120e6f
Megha Bodke <megha.n.bodke@gmail.com>
2023-02-02 22:38:48 +0530

    add imports

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,14 @@
 /* eslint-disable import/extensions */
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import Home from "../components/Home";
-import Details from "../components/Details";
-import Signup from "../components/Signup";
-import Login from "../components/Login";
-import ProtectedRoutes from "../components/ProtectedRoutes";
-import { recieveInstruments } from "../redux/instruments";
-import AddInstrument from "../components/AddInstrument";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import Home from '../components/Home';
+import Details from '../components/Details';
+import Signup from '../components/Signup';
+import Login from '../components/Login';
+import ProtectedRoutes from '../components/ProtectedRoutes';
+import { recieveInstruments } from '../redux/instruments';
+import AddInstrument from '../components/AddInstrument';
 
 const AppRoutes = () => {
   const instrumentsList = useSelector((state) => state.instruments);
@@ -36,7 +36,7 @@ const AppRoutes = () => {
           <Route path="/signup" element={<Signup />} />
         </Routes>
       ) : (
-        ""
+        ''
       )}
     </main>
   );
