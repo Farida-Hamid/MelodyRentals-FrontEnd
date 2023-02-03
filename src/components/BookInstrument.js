fc6082365e6c6ff7a4ec3c284d0df44bf3d892ac
Elie Harfouche <elie7795@gmail.com>
2023-02-03 20:03:52 +0200

    Update BookInstrument.js

--- src/components/BookInstrument.js
+++ src/components/BookInstrument.js
@@ -1,43 +1,49 @@
-import DatePicker from "react-datepicker";
-import {useState} from 'react';
-import {addInstrument, reserveInstrument} from "../api/api";
-import {useDispatch} from "react-redux";
+import DatePicker from 'react-datepicker';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { reserveInstrument } from '../api/api';
 
-const BookInstrument = (props) => {
-    const user_id = JSON.parse(localStorage.getItem('user')).user_id;
-    const instrument_id = props.id;
-    const [startDate, setStartDate] = useState(null);
-    const [endDate, setEndDate] = useState(null);
-    const dispatch = useDispatch();
-    const onChange = (dates) => {
-        const [start, end] = dates;
-        setStartDate(start);
-        setEndDate(end);
-    };
+const BookInstrument = ({ id }) => {
+  const { user_id: userId } = JSON.parse(localStorage.getItem('user'));
+  const instrumentId = id;
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const dispatch = useDispatch();
+  const onChange = (dates) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+  };
 
-    const bookInstrument = () => {
-        event.preventDefault();
-        const reservation = {};
-        reservation.user_id = user_id;
-        reservation.instrument_id = instrument_id;
-        reservation.pickup_date = startDate;
-        reservation.return_date = endDate;
-        dispatch(reserveInstrument(reservation));
-    }
+  const bookInstrument = (event) => {
+    event.preventDefault();
+    const reservation = {};
+    reservation.user_id = userId;
+    reservation.instrument_id = instrumentId;
+    reservation.pickup_date = startDate;
+    reservation.return_date = endDate;
+    dispatch(reserveInstrument(reservation));
+  };
 
-    return (
-        <>
-            <DatePicker
-                selected={startDate}
-                onChange={onChange}
-                startDate={startDate}
-                endDate={endDate}
-                selectsRange
-                inline
-            />
-            <button className="btn btn-primary" onSubmit={bookInstrument}>Book</button>
-        </>
-    );
+  return (
+    <>
+      <DatePicker
+        selected={startDate}
+        onChange={onChange}
+        startDate={startDate}
+        endDate={endDate}
+        selectsRange
+        inline
+      />
+      <button
+        type="button"
+        className="btn btn-primary"
+        onSubmit={bookInstrument}
+      >
+        Book
+      </button>
+    </>
+  );
 };
 
 export default BookInstrument;
