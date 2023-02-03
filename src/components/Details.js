c0a3fac58885d282460b7dbe7dfe0d70e9e607af
Elie Harfouche <elie7795@gmail.com>
2023-02-03 20:04:03 +0200

    Update Details.js

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,18 +1,26 @@
 /* eslint-disable react/jsx-one-expression-per-line */
-import {useLocation, Link, useNavigate} from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { deleteInstrument } from '../api/api';
 
-const DeleteButton = (props) => {
+const DeleteButton = ({ id }) => {
   const user = JSON.parse(localStorage.getItem('user'));
   const dispatch = useDispatch();
   const navigate = useNavigate();
   if (user !== null && user.role === 'admin') {
     return (
-        <button className="btn btn-danger btn-block" onClick={() => dispatch(deleteInstrument(props.id, navigate))}>
+      <>
+        <button
+          type="button"
+          className="btn btn-danger btn-block"
+          onClick={() => {
+            dispatch(deleteInstrument(id, navigate));
+          }}
+        >
           Delete Instrument
         </button>
+      </>
     );
   }
   return <></>;
@@ -20,26 +28,31 @@ const DeleteButton = (props) => {
 
 const Details = () => {
   const location = useLocation();
-  const {
-    instrument,
-    // name, description, image, category, price,
-  } = location.state;
+  const { instrument } = location.state;
 
   return (
-    <div className="homepage">
-      <div className="card border-light">
-        <h1 className="card-title fw-bold text-center">{instrument.name}</h1>
-        <img src={instrument.image} alt={instrument.name} />
-        <p>{instrument.description}</p>
-        <p>
-          Category:
-          {instrument.category}
-        </p>
-        <p>
-          Price/day:
-          {instrument.price}$
-        </p>
-        <DeleteButton id={instrument.id} />
+    <div className="homepage d-flex justify-content-center w-100">
+      <div className="card" style={{ width: '20rem' }}>
+        <div className="card-header">{instrument.name}</div>
+        <img
+          src={instrument.image}
+          className="card-img-top"
+          alt={instrument.name}
+        />
+        <div className="card-body">
+          <p className="card-text mb-5">{instrument.description}</p>
+          <DeleteButton id={instrument.id} />
+        </div>
+        <div className="card-footer d-flex gap-3">
+          <p>
+            Category:
+            {instrument.category}
+          </p>
+          <p>
+            Price/day:
+            {instrument.price}$
+          </p>
+        </div>
       </div>
     </div>
   );
